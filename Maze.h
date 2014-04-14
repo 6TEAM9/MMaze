6ea56e2831c2792d69a1c522fb7737affc05c01d
vladshablinsky <vladshablinsky@gmail.com>
2014-04-14 16:32:24 +0300

    done

--- Maze.h
+++ Maze.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAZE_H
 #define MAZE_H
 #include <vector>
@@ -20,45 +19,6 @@ private:
     void dfs(int x, int y);
     void makeEdge(int x, int y, int dir);
     void makeField();
-=======
-#ifndef MAZE_H_INCLUDED
-
-#include <stdio.h>
-
-using namespace std;
-
-const int debug = 1;
-
-const char EMPTY = 0;
-const char FILLED = 1;
-
-struct Cell{
-	int row;
-	int col;
-	Cell(int _row, int _col) {
-		row = _row;
-		col = _col;
-	}
-};
-
-const Cell DIRS[4] = {
-	{1, 0},
-	{0, 1},
-	{-1, 0},
-	{0, -1}
-};
-
-template <class T> class Queue {
-	T *a;
-	int head;
-	int tail;
-	///TODO: implement or remove
-};
-
-class Maze {
-	Cell m_size;
-	char **m_f;
->>>>>>> 86227d30e8bb0e68725e616289d1215142807113
 public:
     Maze();
     Maze(int n, int m);
